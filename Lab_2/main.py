31caeef920f858393905e1c4ab9f307f57b4d142
Mykhailo Holub <mishataube2005@gmail.com>
2024-03-06 10:52:22 +0200

    insertions_sort reworked

--- Lab_2/main.py
+++ Lab_2/main.py
@@ -1,15 +1,19 @@
+import PythonTableConsole
+import copy
+
 def insertions_sort(input_list):
-    sorted_list = [input_list[0]]
-    for i in range(1,len(input_list)):
-        selected_value = input_list[i]
+    list_to_sort = copy.copy(input_list)
+    for i in range(len(list_to_sort)):
         inserted = False
-        for ii in range(i):
-            if selected_value<sorted_list[ii]:
-                inserted = True
-                sorted_list.insert(ii, selected_value)
+        for ii in range(i-1,-1,-1):
+            if list_to_sort[i] >= list_to_sort[ii]:
+                list_to_sort.insert(ii+1, list_to_sort[i])
+                list_to_sort.pop(i + 1)
+                inserted=True
                 break
         if not inserted:
-            sorted_list.append(selected_value)
-    return sorted_list
+            list_to_sort.insert(0, list_to_sort[i])
+            list_to_sort.pop(i+1)
+    return list_to_sort
 
-print(insertions_sort([-1,10,20,3,40,-40]))
+print(insertions_sort([1,3,2,0,-3,4]))
