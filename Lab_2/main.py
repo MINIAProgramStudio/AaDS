dda77e0ba25ed8a92d7004b8ec5e5abad119dff3
Mykhailo Holub <mishataube2005@gmail.com>
2024-03-06 11:50:05 +0200

    shells sort

--- Lab_2/main.py
+++ Lab_2/main.py
@@ -1,8 +1,16 @@
 import PythonTableConsole
 import copy
+import math
+import random
 
-unsorted_list = [1,4,2,7,-1,0,3,5]  # без повторів!!! Повтори ламають сортування :(
-''' не подивився варіант (:
+def unsortgen(length):
+    unsorted_list = list(range(length))
+    for i in range(int(length/2)):
+        pos_1 = int(random.random() * length)
+        pos_2 = int(random.random() * length)
+        unsorted_list[pos_1], unsorted_list[pos_2] = unsorted_list[pos_2], unsorted_list[pos_1]
+    return unsorted_list
+
 def insertions_sort(input_list):
     list_to_sort = copy.copy(input_list)
     for i in range(len(list_to_sort)):
@@ -38,7 +46,7 @@ def logged_insertions_sort(input_list):
         for ii in range(len(list_to_sort)):
             Log_Table.contains[ii].append(list_to_sort[ii])
     return Log_Table
-'''
+
 
 def selections_sort(input_list):
     list_to_sort = copy.copy(input_list)
@@ -59,4 +67,43 @@ def logged_selections_sort(input_list):
         for ii in range(len(list_to_sort)):
             log_table.contains[ii].append(list_to_sort[ii])
     return log_table
-print(logged_selections_sort(unsorted_list))
+
+def shells_sort(input_list):
+    list_to_sort = copy.copy(input_list)
+    hk = [1]
+    while True:
+        new_hk_value = hk[0]*3+1
+        if new_hk_value<len(input_list):
+            hk.insert(0,new_hk_value)
+        else:
+            break
+    for iteration in range(len(hk)):
+        for i in range(hk[iteration]):
+            sublist = list_to_sort[slice(i,len(list_to_sort),hk[iteration])]
+            sublist = selections_sort(sublist)
+            for ii in range(len(sublist)):
+                list_to_sort[ii*hk[iteration]+i] = sublist[ii]
+    return list_to_sort
+
+def logged_shells_sort(input_list):
+    log_table = PythonTableConsole.PythonTableConsole([input_list])
+    log_table.transpose()
+    list_to_sort = copy.copy(input_list)
+    hk = [1]
+    while True:
+        new_hk_value = hk[0]*3+1
+        if new_hk_value<len(input_list):
+            hk.insert(0,new_hk_value)
+        else:
+            break
+    for iteration in range(len(hk)):
+        for i in range(hk[iteration]):
+            sublist = list_to_sort[slice(i,len(list_to_sort),hk[iteration])]
+            sublist = selections_sort(sublist)
+            for ii in range(len(sublist)):
+                list_to_sort[ii*hk[iteration]+i] = sublist[ii]
+        for i in range(len(list_to_sort)):
+            log_table.contains[i].append(list_to_sort[i])
+    return log_table
+
+print(logged_shells_sort(unsortgen(10)))
