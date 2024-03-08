8851a73b9133b71c57c234cfbefd5993a1e84261
Mykhailo Holub <mishataube2005@gmail.com>
2024-03-08 10:44:23 +0200

    AAAAAAAAAAAAAAAAAAAAAA

--- Lab_2/main.py
+++ Lab_2/main.py
@@ -10,6 +10,20 @@ def unsortgen(length):
         unsorted_list[pos_1], unsorted_list[pos_2] = unsorted_list[pos_2], unsorted_list[pos_1]
     return unsorted_list
 
+def insertions_sort(input_list):
+    list_to_sort = copy.copy(input_list)
+    for i in range(len(list_to_sort)):
+        inserted = False
+        for ii in range(i-1,-1,-1):
+            if list_to_sort[i] >= list_to_sort[ii]:
+                list_to_sort.insert(ii+1, list_to_sort[i])
+                list_to_sort.pop(i + 1)
+                inserted=True
+                break
+        if not inserted:
+            list_to_sort.insert(0, list_to_sort[i])
+            list_to_sort.pop(i+1)
+    return list_to_sort
 
 def selections_sort(input_list):
     compared = 0
@@ -35,20 +49,28 @@ def shells_sort(input_list):
             hk.insert(0, new_hk_value)
         else:
             break
-    for iteration in range(len(hk)):
-        for i in range(hk[iteration]):
-            sublist = slice(i, len(list_to_sort), hk[iteration])
-            for ii in range(len(list_to_sort[sublist])):
-                minimum = min(list_to_sort[sublist][ii:])
-                compared += len(list_to_sort[sublist][ii:])
-                index_of_minimum = list_to_sort[sublist].index(minimum)*hk[iteration]+i
-                index_of_start = i+ii*hk[iteration]
-                list_to_sort[index_of_minimum], list_to_sort[index_of_start] = list_to_sort[index_of_start], list_to_sort[index_of_minimum]
-                moved+=1
+    for gap in hk:
+        for offset in range(gap):
+            sublist = list_to_sort[slice(offset, len(list_to_sort), gap)]
+            print('before:')
+            print(sublist)
+            for unsorted_element in range(0,len(sublist)):
+                inserted = False
+                for sorted_element in range(unsorted_element):
+                    if sublist[unsorted_element] < sublist[sorted_element]:
+                        sublist.insert(sorted_element, sublist[unsorted_element])
+                        sublist.pop(unsorted_element+1)
+
+                if not inserted:
+                    sublist.append(sublist[unsorted_element])
+                    sublist.pop(unsorted_element)
+            print('after:')
+            print(sublist)
+
     return [list_to_sort,compared,moved]
 
 print('Сортування списку довжини 100')
-unsorted_list = unsortgen(100)
+unsorted_list = unsortgen(10)
 print('Список: '+str(unsorted_list))
 start = time.time()
 sel_sor_100 = selections_sort(unsorted_list)
@@ -72,7 +94,7 @@ print('Сортування методом Шелла:')
 print('Порівнянь: '+str(she_sor_100[1]))
 print('Обмінів: '+str(she_sor_100[2]))
 print('Час виконання: '+str(she_sor_100[3]))
-
+"""
 print('Сортування списку довжини 1000')
 unsorted_list = unsortgen(1000)
 start = time.time()
@@ -118,6 +140,7 @@ print('Сортування методом Шелла:')
 print('Порівнянь: '+str(she_sor_10k[1]))
 print('Обмінів: '+str(she_sor_10k[2]))
 print('Час виконання: '+str(she_sor_10k[3]))
+"""
 """
 print('Сортування списку довжини 100k')
 unsorted_list = unsortgen(100000)
