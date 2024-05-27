a0029e3cb732c80299dc5959e248056c08e0c73a
Mykhailo Holub <mishataube2005@gmail.com>
2024-05-27 20:23:45 +0300

    formatting

--- lab_5/Node.py
+++ lab_5/Node.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, value, parent = None, right_arm = None, left_arm = None):
+    def __init__(self, value, parent=None, right_arm=None, left_arm=None):
         self.value = value
         self.parent = parent
         self.right_arm = right_arm
