dff491a15179610e10edc08d18bbf95e9655a910
Mykhailo Holub <mishataube2005@gmail.com>
2024-05-27 20:23:37 +0300

    draw(self) implemented in Tree

--- lab_5/Tree.py
+++ lab_5/Tree.py
@@ -1,4 +1,6 @@
 from Node import Node
+import turtle
+import copy
 
 class Tree:
     def __init__(self, root):
@@ -17,4 +19,44 @@ class Tree:
             else:
                 return selected_node
             if selected_node is None:
+                return 0
+
+    def draw(self):
+        previous_level = []
+        level = [self.root]
+        level_counter = 0
+        while True:
+            # draw level
+            turtle.penup()
+            turtle.goto(-15 * (len(level) - 1), level_counter * 15)
+            for node in level:
+                if not node is None:
+                    turtle.write(node.value, align="center")
+                    turtle.setheading(90)
+                    turtle.forward(30)
+
+            # draw connections
+            for node in level:
+                if not node is None:
+                    if not node.parent is None:
+                        turtle.penup()
+                        turtle.goto(-15 * (len(level) - 1) + 30 * level.index(node), level_counter * 15)
+                        turtle.pendown()
+                        turtle.goto(-15 * (len(previous_level) - 1) + 30 * previous_level.index(node.parent), level_counter * 15 - 15)
+
+            # iterate
+            nodes_exist = False
+            new_level = []
+            for node in level:
+                if not node is None:
+                    new_level.append(node.left_arm)
+                    new_level.append(node.right_arm)
+                    nodes_exist = True
+                else:
+                    new_level.append(None)
+                    new_level.append(None)
+            previous_level = level
+            level = new_level
+
+            if not nodes_exist:
                 return 0
